b655cc4dc9d8077b24c7a09fce1e5fc16bd876d7
mferdinandr <mferdinr@gmail.com>
2025-05-27 22:23:57 +0700

    feat: reset password page

--- website/resources/js/pages/auth/forgot-password.tsx
+++ website/resources/js/pages/auth/forgot-password.tsx
@@ -22,13 +22,13 @@ export default function ForgotPassword({ status }: { status?: string }) {
     };
 
     return (
-        <AuthLayout title="Forgot password" description="Enter your email to receive a password reset link">
+        <AuthLayout heading1="Reset Your" heading2="Password!" imagePath="/book_in_hand.png">
             <Head title="Forgot password" />
 
             {status && <div className="mb-4 text-center text-sm font-medium text-green-600">{status}</div>}
 
             <div className="space-y-6">
-                <form onSubmit={submit}>
+                <form onSubmit={submit} className="bg-on-background flex w-85 flex-col gap-1 rounded-lg px-5 py-3 sm:w-96">
                     <div className="grid gap-2">
                         <Label htmlFor="email">Email address</Label>
                         <Input
@@ -45,17 +45,20 @@ export default function ForgotPassword({ status }: { status?: string }) {
                         <InputError message={errors.email} />
                     </div>
 
-                    <div className="my-6 flex items-center justify-start">
-                        <Button className="w-full" disabled={processing}>
+                    <div className="my-3 flex items-center justify-start">
+                        <Button variant={'custom'} className="w-full" disabled={processing}>
                             {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
                             Email password reset link
                         </Button>
                     </div>
                 </form>
 
-                <div className="text-muted-foreground space-x-1 text-center text-sm">
-                    <span>Or, return to</span>
-                    <TextLink href={route('login')}>log in</TextLink>
+                <div className="text-muted-foreground space-x-1 text-end text-sm">
+                    <span className="font-light">
+                        Or, return to
+                        <br />
+                    </span>
+                    <TextLink href={route('login')}>Log in</TextLink>
                 </div>
             </div>
         </AuthLayout>
