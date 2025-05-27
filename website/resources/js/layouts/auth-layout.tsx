7594acd1bbb395a48cdc85a733ba4d22bde87a5e
mferdinandr <mferdinr@gmail.com>
2025-05-27 22:23:19 +0700

    feat: auth layout

--- website/resources/js/layouts/auth-layout.tsx
+++ website/resources/js/layouts/auth-layout.tsx
@@ -1,9 +1,27 @@
 import AuthLayoutTemplate from '@/layouts/auth/auth-simple-layout';
 
-export default function AuthLayout({ children, title, description, ...props }: { children: React.ReactNode; title: string; description: string }) {
+type AuthLayoutProps = {
+    title?: string;
+    children: React.ReactNode;
+    description?: string;
+    heading1?: string;
+    heading2?: string;
+    imagePath?: string;
+};
+
+export default function AuthLayout({ children, title, description, heading1, heading2, imagePath, ...props }: AuthLayoutProps) {
     return (
-        <AuthLayoutTemplate title={title} description={description} {...props}>
-            {children}
-        </AuthLayoutTemplate>
+        <div className="bg-secondary flex h-screen w-screen flex-col items-center md:flex-row">
+            <div className="48508D text-on-background flex h-1/4 w-screen flex-col justify-center bg-gradient-to-r from-[#141627] to-[#48508D] text-lg md:h-screen md:w-1/2">
+                <p className="px-4 text-6xl sm:pl-20">
+                    {heading1}
+                    <br /> <span className="font-heading">{heading2}</span>
+                </p>
+                <img src={`/image/vector/${imagePath}`} alt="Book in Hand" className="hidden w-1/2 place-self-center md:block" />
+            </div>
+            <AuthLayoutTemplate title={title} description={description} {...props} className="md:w-1/2">
+                <div>{children}</div>
+            </AuthLayoutTemplate>
+        </div>
     );
 }
--- website/resources/js/layouts/auth/auth-simple-layout.tsx
+++ website/resources/js/layouts/auth/auth-simple-layout.tsx
@@ -1,4 +1,3 @@
-import AppLogoIcon from '@/components/app-logo-icon';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
 
@@ -6,27 +5,29 @@ interface AuthLayoutProps {
     name?: string;
     title?: string;
     description?: string;
+    className?: string;
 }
 
-export default function AuthSimpleLayout({ children, title, description }: PropsWithChildren<AuthLayoutProps>) {
+export default function AuthSimpleLayout({ children, title, description, className }: PropsWithChildren<AuthLayoutProps>) {
     return (
-        <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <div className="w-full max-w-sm">
-                <div className="flex flex-col gap-8">
+        <div className={`flex items-center justify-center gap-6 ` + className}>
+            <div className="max-w-sm">
+                <div className="flex flex-col gap-1">
                     <div className="flex flex-col items-center gap-4">
                         <Link href={route('home')} className="flex flex-col items-center gap-2 font-medium">
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                                <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
-                            </div>
                             <span className="sr-only">{title}</span>
                         </Link>
-
-                        <div className="space-y-2 text-center">
-                            <h1 className="text-xl font-medium">{title}</h1>
-                            <p className="text-muted-foreground text-center text-sm">{description}</p>
+                        <div className="text-center">
+                            <img src="/image/logo.png" alt="Book in Hand" className="w-24" />
+                            {title && (
+                                <>
+                                    <h1 className="text-xl font-medium">{title}</h1>
+                                    <p className="text-muted-foreground text-center text-sm">{description}</p>
+                                </>
+                            )}
                         </div>
                     </div>
-                    {children}
+                    <div>{children}</div>
                 </div>
             </div>
         </div>
