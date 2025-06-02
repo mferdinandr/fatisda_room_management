9677474ec617adcf54afae973394e96b797cf4ec
mferdinandr <mferdinr@gmail.com>
2025-06-02 23:32:54 +0700

    feat: sidebar calender

---
+++ website/resources/js/components/Client/Sidebar/Sidebar.tsx
@@ -0,0 +1,45 @@
+import { Calendar } from '@/components/ui/calendar';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { useState } from 'react';
+
+const Sidebar = () => {
+    const [date, setDate] = useState<Date | undefined>(new Date());
+    const [isOpen, setIsOpen] = useState(true);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div
+            className={`bg-background absolute top-28 bottom-0 left-0 flex flex-col transition-all duration-300 ease-in-out ${isOpen ? 'w-70 p-3 md:p-4' : 'w-14 p-2'} rounded-tr-2xl`}
+        >
+            <div className={`${isOpen ? 'mx-0' : 'mx-auto'} mt-2 flex`}>
+                {isOpen && <p className="mb-2 font-light tracking-wider opacity-40">Select Date</p>}
+                <div className="ml-auto">
+                    {isOpen ? (
+                        <ChevronLeft onClick={toggleSidebar} className="cursor-pointer" />
+                    ) : (
+                        <ChevronRight onClick={toggleSidebar} className="cursor-pointer" />
+                    )}
+                </div>
+            </div>
+
+            {isOpen && (
+                <>
+                    <h3 className="mb-4 text-xl text-white">
+                        {date?.toLocaleDateString('en-US', {
+                            weekday: 'long',
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric',
+                        })}
+                    </h3>
+                    <Calendar mode="single" selected={date} onSelect={setDate} className="" />
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Sidebar;
--- website/resources/js/pages/Home/index.tsx
+++ website/resources/js/pages/Home/index.tsx
@@ -1,4 +1,5 @@
-import Navbar from '@/components/Client/Navbar';
+import Navbar from '@/components/Client/Navbar/Navbar';
+import Sidebar from '@/components/Client/Sidebar/Sidebar';
 import { Head } from '@inertiajs/react';
 
 export default function Welcome() {
@@ -8,16 +9,11 @@ export default function Welcome() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] px-6 py-4 text-[#1b1b18] lg:justify-center lg:px-8">
+            <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] px-3 py-4 text-[#1b1b18] md:px-8 lg:justify-center">
                 <Navbar />
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
                     <main className="flex w-full max-w-[335px] flex-col-reverse text-white lg:max-w-4xl lg:flex-row">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, reprehenderit assumenda! Ex, quos itaque? Facilis animi
-                        libero omnis a, rem perferendis in amet, impedit ea, magni quas quia culpa vitae accusamus molestiae harum! Recusandae
-                        officiis perferendis, laudantium odit excepturi a? Quos, quasi maiores commodi debitis nostrum eum ea quisquam voluptatem
-                        velit corrupti corporis impedit quo atque libero quas doloribus eius rem odit dolor eaque neque ipsa accusantium? Atque quis
-                        veniam, vero perspiciatis at rem possimus officia consequuntur eum cum dolorum alias laudantium doloremque eius distinctio
-                        labore iste? Officiis non, vitae ullam in quasi numquam, cupiditate quae, quas corrupti quis aut?
+                        <Sidebar />
                     </main>
                 </div>
                 <div className="hidden h-14.5 lg:block"></div>
