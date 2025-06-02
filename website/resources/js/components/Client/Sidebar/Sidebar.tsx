4adf1906a2a0370e5955fc85682eb8a67897d887
mferdinandr <mferdinr@gmail.com>
2025-06-02 23:38:54 +0700

    Merge branch 'main' of https://github.com/mferdinandr/fatisda_room_management

--- website/resources/js/components/Client/Sidebar/Sidebar.tsx
+++ website/resources/js/components/Client/Sidebar/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
 
     return (
         <div
-            className={`bg-background absolute top-28 bottom-0 left-0 flex flex-col transition-all duration-300 ease-in-out ${isOpen ? 'w-70 p-3 md:p-4' : 'w-14 p-2'} rounded-tr-2xl`}
+            className={`bg-background absolute top-28 bottom-0 left-0 flex flex-col transition-all duration-300 ease-in-out ${isOpen ? 'w-70 p-3 md:p-4' : 'w-14 p-2'} rounded-tr-4xl`}
         >
             <div className={`${isOpen ? 'mx-0' : 'mx-auto'} mt-2 flex`}>
                 {isOpen && <p className="mb-2 font-light tracking-wider opacity-40">Select Date</p>}
