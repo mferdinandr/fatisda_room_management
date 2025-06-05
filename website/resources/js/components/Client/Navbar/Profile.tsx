eec61de13a2a1d026c295edf070109825ec2d819
mferdinandr <mferdinr@gmail.com>
2025-06-05 09:16:42 +0700

    feat: logout button

--- website/resources/js/components/Client/Navbar/Profile.tsx
+++ website/resources/js/components/Client/Navbar/Profile.tsx
@@ -1,5 +1,7 @@
+import { Button } from '@/components/ui/button';
 import { router } from '@inertiajs/react';
 import { EllipsisVertical, User } from 'lucide-react';
+import { useState } from 'react';
 
 type IProfileProps = {
     name: string;
@@ -7,8 +9,21 @@ type IProfileProps = {
 };
 
 const Profile = ({ name, email }: IProfileProps) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     const handleLogout = () => {
+        // Tampilkan modal konfirmasi logout
+        setIsModalOpen(true);
+    };
+
+    const confirmLogout = () => {
+        // Proses logout setelah konfirmasi
         router.post('/logout');
+    };
+
+    const cancelLogout = () => {
+        // Menutup modal tanpa logout
+        setIsModalOpen(false);
     };
 
     return (
@@ -33,6 +48,23 @@ const Profile = ({ name, email }: IProfileProps) => {
                     </div>
                 </div>
             </div>
+
+            {isModalOpen && (
+                <div className="bg-opacity-50 fixed inset-0 z-50 flex items-center justify-center bg-black/30">
+                    <div className="w-2/3 max-w-96 rounded-lg bg-white p-6 shadow-lg">
+                        <h3 className="font-sans text-2xl font-bold">Are you sure?</h3>
+                        <p className="my-2 text-sm">Do you want logout?</p>
+                        <div className="mt-5 flex justify-end gap-4">
+                            <Button onClick={cancelLogout} className="rounded-md border px-4 py-2 text-sm text-gray-600 hover:bg-gray-100">
+                                Cancel
+                            </Button>
+                            <Button onClick={confirmLogout} variant={'destructive'}>
+                                Logout
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
